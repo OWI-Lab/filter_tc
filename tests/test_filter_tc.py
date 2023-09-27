b48e68f82e5e52329eb00cab1981127821fcf50b
wweijtje <85878088+wweijtje@users.noreply.github.com>
2023-09-27 12:40:36 +0200

    I prefer to have all our codes on the pytest framework

--- tests/test_filter_tc.py
+++ tests/test_filter_tc.py
@@ -1,33 +1,9 @@
-#!/usr/bin/env python
-
-"""Tests for `filter_tc` package."""
+# PyTests for tc
 
 
-import unittest
-from click.testing import CliRunner
 
-from filter_tc import sdypy_particle_filter
-from filter_tc import cli
+def test_output_sep005():
+    assert True
 
-
-class TestFilter_tc(unittest.TestCase):
-    """Tests for `filter_tc` package."""
-
-    def setUp(self):
-        """Set up test fixtures, if any."""
-
-    def tearDown(self):
-        """Tear down test fixtures, if any."""
-
-    def test_000_something(self):
-        """Test something."""
-
-    def test_command_line_interface(self):
-        """Test the CLI."""
-        runner = CliRunner()
-        result = runner.invoke(cli.main)
-        assert result.exit_code == 0
-        assert 'filter_tc.cli.main' in result.output
-        help_result = runner.invoke(cli.main, ['--help'])
-        assert help_result.exit_code == 0
-        assert '--help  Show this message and exit.' in help_result.output
+def test_filter_performance():
+    assert True
